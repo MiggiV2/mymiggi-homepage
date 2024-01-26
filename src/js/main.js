20d311347521179e6eb37c186c573e1b66d1adf5
Miggi <miggi@noreply.familyhainz.de>
2024-01-26 19:59:20 +0100

    :lipstick: CSS & mobile improvements

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,13 @@
+import { Collapse } from "bootstrap";
+
 const home = document.getElementById("center-home");
 const about = document.getElementById("center-about");
 const contact = document.getElementById("center-contact");
 const aboutLink = document.getElementById("about-link");
+const aboutLink2 = document.getElementById("about-link-2");
 const homeLink = document.getElementById("home-link");
 const contactLink = document.getElementById("contact-link");
+const contactLink2 = document.getElementById("contact-link-2");
 
 init();
 
@@ -11,8 +15,10 @@ function init() {
   autoShow();
 
   aboutLink.addEventListener("click", showAbout);
+  aboutLink2.addEventListener("click", showAbout);
   homeLink.addEventListener("click", showHome);
   contactLink.addEventListener("click", showContact);
+  contactLink2.addEventListener("click", showContact);
 }
 
 function autoShow() {
